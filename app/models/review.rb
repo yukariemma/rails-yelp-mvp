e94173d150c4a9980aaefe2655b0ee017eccd185
Yukari Yamamoto <yukariemma@gmail.com>
2019-02-20 16:34:12 +0100

    completed restaurants and reviews

---
+++ app/models/review.rb
@@ -0,0 +1,9 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :restaurant, :content, :rating, presence: true
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: (0..5) }
+end
+
+# validates_inclusion_of :number, :in => 1..10
